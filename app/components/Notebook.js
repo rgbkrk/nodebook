cb01953445a6c88ea35d9452f5d709bb87b154ac
Kyle Kelley <rgbkrk@gmail.com>
2018-06-28 10:58:40 -0700

    ok now we are really getting started

---
+++ app/components/Notebook.js
@@ -0,0 +1,8 @@
+// @flow
+import * as React from "react";
+
+export default class Notebook extends React.Component<null, null> {
+  render() {
+    return <div>Notebook</div>;
+  }
+}
--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,13 @@
 /* eslint flowtype-errors/show-errors: 0 */
-import React from 'react';
-import { Switch, Route } from 'react-router';
-import App from './containers/App';
-import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import React from "react";
+import { Switch, Route } from "react-router";
+import App from "./containers/App";
+import NotebookPage from "./containers/NotebookPage";
 
 export default () => (
   <App>
     <Switch>
-      <Route path="/counter" component={CounterPage} />
-      <Route path="/" component={HomePage} />
+      <Route path="/" component={NotebookPage} />
     </Switch>
   </App>
 );
