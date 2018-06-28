fd43b671dc9a4313664f24835e94c615e5aab609
Kyle Kelley <rgbkrk@gmail.com>
2018-06-28 11:08:10 -0700

    start up a notebook

--- app/components/Notebook.js
+++ app/components/Notebook.js
@@ -1,8 +1,47 @@
 // @flow
 import * as React from "react";
+import {
+  Cell as CellContainer,
+  Source,
+  Cells,
+  Input,
+  Prompt
+} from "@nteract/presentational-components";
 
-export default class Notebook extends React.Component<null, null> {
+type CellID = string;
+type Cell = {};
+
+type AppState = {
+  notebook: {
+    cellList: Array<CellID>
+  },
+  cells: {
+    [CellID]: Cell
+  }
+};
+
+const initialProps = {
+  notebook: {
+    cellList: ["b", "a"]
+  },
+  cells: {
+    b: {},
+    a: {}
+  }
+};
+
+class Notebook extends React.Component<AppState, null> {
   render() {
-    return <div>Notebook</div>;
+    return this.props.notebook.cellList.map(cellID => (
+      <pre key={cellID}>{cellID}</pre>
+    ));
+  }
+}
+
+export default class NotebookApp extends React.Component<null, null> {
+  render() {
+    return (
+      <Notebook notebook={initialProps.notebook} cells={initialProps.cells} />
+    );
   }
 }
